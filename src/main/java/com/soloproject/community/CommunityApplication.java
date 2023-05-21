d9ae9abc61cf3b875de3f3624a883dd6cc56d328
서은정 <111293023+seoeunjeong@users.noreply.github.com>
2023-05-22 03:57:30 +0900

    [rename]

--- src/main/java/com/soloproject/community/CommunityApplication.java
+++ src/main/java/com/soloproject/community/CommunityApplication.java
@@ -1,13 +1,10 @@
 package com.soloproject.community;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableBatchProcessing
 @SpringBootApplication
 public class CommunityApplication {
 
